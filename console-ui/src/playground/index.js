93c56f4b1e439f84e0acf74d52a13f550248d49f
Parthkk90 <parth122004@gmail.com>
2026-03-29 21:42:16 +0530

    Remove unused frontend files

--- console-ui/src/playground/index.js
+++
@@ -1,5 +0,0 @@
-// Playground exports
-export { parseDSL, dslToJSON } from './dslParser';
-export { highlightDSL } from './highlighter';
-export { SAMPLE_CARD, SAMPLE_LIST, SAMPLES } from './samples';
-export { default as CodeEditor } from './CodeEditor';
